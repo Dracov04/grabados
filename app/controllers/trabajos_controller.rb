5198455a04c2d8e8862c2a25557de594c1d2bdd2
Dracov04 <dracov.m.l@gmail.com>
2014-11-14 17:41:26 +0100

    New divisions

--- app/controllers/trabajos_controller.rb
+++ app/controllers/trabajos_controller.rb
@@ -1,11 +1,26 @@
 class TrabajosController < ApplicationController
-	def insignias
+	def badges
+		
+	end
+	def cuadros
 		
 	end
 	def emblemas
 		
 	end
+	def heraldica
+		
+	end
+	def laminas
+		
+	end
+	def metopas
+		
+	end
 	def placas
+		
+	end
+	def trofeos
 		
 	end
 end
