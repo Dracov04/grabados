0b018278a6a229cf3eda728177d0739b5019b1ec
Dracov04 <dracov.m.l@gmail.com>
2015-01-27 18:54:33 +0100

    Trying delete assets.rb

--- config/initializers/assets.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.config.assets.precompile += %w( jquery-1.2.6.js )
-Rails.application.config.assets.precompile += %w( jquery-1.2.6.pack.js )
-Rails.application.config.assets.precompile += %w( startstop-slider.js )
